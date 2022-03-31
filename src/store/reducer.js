79f2146d07edaeb65f36c24a865e933747232440
John Toriola <john.toriola.tj@gmail.com>
2022-03-31 11:04:40 +0100

    added server connection

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://checkersdb.herokuapp.com/", {
   reconnection: false,
 });
 socket.on("connect", () => {
